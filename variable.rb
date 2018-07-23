2bcf32adf7a4889f4b762f5112688580aeebc23a
brysona10 <addisonbryson10@gmail.com>
2018-07-23 19:45:31 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,9 @@
-name = gets
+# Take in the user's name as input and output a personalized name
+puts "what is your name?"
+name=gets
 
-puts name
+puts 
+"Hello #(name)"
 
 # name="Arella" # Define a local variable called 'greeting',
 # puts Hello #(name)!
